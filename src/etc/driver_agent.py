a92645f7fc9ecb66dca58e0344958c846883cac9
NB <altmshfkgudtjr@naver.com>
2019-09-21 00:53:02 +0900

    window/linux chromedriver

--- src/etc/driver_agent.py
+++ src/etc/driver_agent.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
-
-
+from platform import platform
 
 def chromedriver():
 	#만약 driver이 켜져있으면 끄고, 없으면 그냥 진행
@@ -16,6 +15,9 @@ def chromedriver():
 	options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5)AppleWebKit 537.36 (KHTML, like Gecko) Chrome")
 	options.add_argument("lang=ko_KR")
 
-	driver = webdriver.Chrome('../chromedriver.exe', chrome_options=options)
-	
+	if platform().startswith("Windows"):
+		driver = webdriver.Chrome('../chromedriver.exe', chrome_options=options)
+	else:
+		driver = webdriver.Chrome('home/iml/SOOJLE_Crawler/chromedriver', chrome_options=options)
+
 	return driver
