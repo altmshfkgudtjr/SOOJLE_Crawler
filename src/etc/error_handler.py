f1f1615d8796966340bc7958a9437489da221c6e
NB <altmshfkgudtjr@naver.com>
2019-09-21 18:25:59 +0900

    log path modified for linux

--- src/etc/error_handler.py
+++ src/etc/error_handler.py
@@ -1,6 +1,7 @@
 from db_health import url_health_check
 from datetime import datetime
 import time
+from platform import platform
 
 def error_handler(e, URL, page_url, db):
 	# 앞으로 10번동안 이 사이트 크롤링 일시중지
@@ -11,7 +12,10 @@ def error_handler(e, URL, page_url, db):
 	print("[ERROR]=====================================================================")
 	print(log_time, " :: ", log_info, "\nURL :: ", log_url)
 	print(type(e), "\n", e, "\n\n\n\n")
-	f = open("crawler_log.log", 'a')
+	if platform().startswith("Windows"):
+		f = open("/home/iml/log/crawler_log.log", 'a')
+	else:
+		f = open("crawler_log.log", 'a')
 	f_data = "[ERROR]=====================================================================\n"
 	f_data = f_data + log_time + " :: " + log_info + "\nURL :: " + log_url + "\n"
 	f_data = f_data + str(type(e)) + "\n" + str(e) + "\n\n"
