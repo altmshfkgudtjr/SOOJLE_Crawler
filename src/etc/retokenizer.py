1534fff39aa0f0325ad057321988481633d4f64f
NB <altmshfkgudtjr@naver.com>
2019-10-06 00:24:24 +0900

    retokenizer modfied

--- src/etc/retokenizer.py
+++ src/etc/retokenizer.py
@@ -19,80 +19,63 @@ import time
 
 HOST = 'localhost:27017'
 
-def show_percent(post_per):
-	if platform().startswith("Windows"):
-		clear = lambda: os.system('cls')
-		clear()
-	else:
-		clear = lambda: os.system('clear')
-	#print(":::: SOOJLE PROJECT RETOKENIZER! ::::\n")
-	#print(">>> RETOKENIZING...\n\n")
-	print(post_per, "%")
-	for i in range(post_per):
-		print("■", end="")
-	print()
-
-def retokenizer(ID, PW):
+def retokenizer(ID, PW, BOARD):
 	client = MongoClient('mongodb://%s:%s@%s' %(ID, PW, HOST))
 	db = client["soojle"]
 	cnt = 0
-	posts_cnt = db.posts.find().count()
-	posts = db.posts.find()
+	posts_cnt = db[BOARD].find().count()
+	posts = db[BOARD].find()
 	post_per = 0
 	for post in posts:
 		cnt += 1
 		print(cnt)
-		#post_per = (cnt / (posts_cnt / 100))
-		#if int(post_per * 10) % 10 == 0:
-		#	show_percent(int(post_per / 10))
 		target_id = post["_id"]
 		target_token = soojle_tokenize(post["title"], post["post"])
-		db.posts.update_one({"_id": ObjectId(target_id)}, {"$set": {"token": target_token}})
+		db[BOARD].update_one({"_id": ObjectId(target_id)}, {"$set": {"token": target_token}})
 	print("\n\n:::: ", cnt, " POSTS RETOKENIZING DONE! ::::\n\n")
 	client.close()
 
 
-def retokenizer_everytime(ID, PW):
+def retokenizer_everytime(ID, PW, BOARD):
 	client = MongoClient('mongodb://%s:%s@%s' %(ID, PW, HOST))
 	db = client["soojle"]
 	cnt = 0
-	posts_cnt = db.posts.find().count()
-	posts = db.posts.find({"info": {"$regex": "everytime"}})
+	posts_cnt = db[BOARD].find().count()
+	posts = db[BOARD].find({"info": {"$regex": "everytime"}})
 	post_per = 0
 	show_percent(post_per)
 	for post in posts:
 		cnt += 1
 		print(cnt)
-		#post_per = (posts_cnt / cnt) % 10
-		#if int(post_per * 10) % 10 == 0:
-		#	show_percent(int(post_per / 10))
 		target_id = post["_id"]
 		target_token = soojle_tokenize(post["title"], post["post"])
 		post["token"] = target_token
 		del post["_id"]
 		del post["title_token"]
 		post["title"] = "0"
-		db.posts.replace_one({"_id": ObjectId(target_id)}, post)
+		db[BOARD].replace_one({"_id": ObjectId(target_id)}, post)
 	print("\n\n:::: ", cnt, " POSTS RETOKENIZING DONE! ::::\n\n")
 	client.close()
 
 if __name__ == '__main__':
 	IS_EVERYTIME = False
-	if len(sys.argv) == 3:
+	if len(sys.argv) == 4:
 		ID = sys.argv[1]
 		PW = sys.argv[2]
-	elif len(sys.argv) == 4:
+		BOARD = sys.argv[3]
+	elif len(sys.argv) == 5:
 		ID = sys.argv[1]
 		PW = sys.argv[2]
 		if sys.argv[3] == "1" or sys.argv[3] == "True":
 			IS_EVERYTIME = True
+		BOARD = sys.argv[4]
 	else:
 		print(":::: WRONG INPUT! ::::\n\n\n")
 	if IS_EVERYTIME == True:
 		print("POSTS = EVERYTIME")
 		time.sleep(3)
-		retokenizer_everytime(ID, PW)
+		retokenizer_everytime(ID, PW, BOARD)
 	else:
 		print("POSTS = ALL")
 		time.sleep(3)
-		retokenizer(ID, PW)
+		retokenizer(ID, PW, BOARD)
