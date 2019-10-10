4b333ee7bf051d69d21e3bc4d81026c6b0dba59c
NB <altmshfkgudtjr@naver.com>
2019-10-10 20:08:40 +0900

    time loggin modified

--- src/etc/timeLogWrite.py
+++ src/etc/timeLogWrite.py
@@ -17,7 +17,7 @@ def time_write(start_time, end_time, db, posts_len):
 		f = open("./log/crawler_time.log", 'a')
 	else:
 		f = open("/home/iml/log/crawler_time.log", 'a')
-	f_data += "END   : " + end_time + "\n"
+	f_data = "END   : " + end_time + "\n"
 	f_data += "RUN   : " + str(running_time) + "\n"
 	f_data += "END_DATA   : " + str(posts_data) + "\n"
 	f_data += "CRAWLED_DATA    : " + str(posts_data - posts_len) + "\n\n\n\n\n"
