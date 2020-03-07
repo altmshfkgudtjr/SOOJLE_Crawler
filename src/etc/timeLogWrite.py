8312ecc219679c251fc240516920c08ad0a00026
NB <altmshfkgudtjr@naver.com>
2020-03-07 17:28:52 +0900

    log category classify

--- src/etc/timeLogWrite.py
+++ src/etc/timeLogWrite.py
@@ -36,6 +36,28 @@ def log_write(start_time, end_time, db, BEFORE_DATA):
 	classification_crawling_sort = list(filter(lambda x: x['count'] != 0, crawling_classification_all))
 	classification_crawling_sort.sort(key=lambda cnt: cnt['count'] ,reverse=True)
 	classification_all.sort(key=lambda cnt: cnt['count'] ,reverse=True)
+	# Category info Get
+	category_all = db.category_of_topic.find({}, {"_id": False, "category_name": True, "info": True})
+	category_all = list(category_all)
+	# Crawling Category
+	for category in category_all:
+		category['info'] = list(map(lambda x: x.split("_")[1] + "_" + x.split("_")[2], category['info']))
+		category['count'] = 0
+	category_total = copy.deepcopy(category_all)
+	for info_crawling in classification_crawling_sort:
+		for category in category_all:
+			if info_crawling['_id'] in category['info']:
+				category['count'] += info_crawling['count']
+	for category in category_all:
+		del category['info']
+	category_all = list(filter(lambda x: x['count'] != 0, category_all))
+	# Total Category
+	for info_data in classification_all:
+		for category in category_total:
+			if info_data['_id'] in category['info']:
+				category['count'] += info_data['count']
+	for category in category_total:
+		del category['info']
 
 
 	# Shell 출력
@@ -67,7 +89,9 @@ def log_write(start_time, end_time, db, BEFORE_DATA):
 			'hidden_posts': hidden_posts_data
 		},
 		"info_crawling": classification_crawling_sort,
-		"info_data": classification_all
+		"info_data": classification_all,
+		"category_crawling": category_all,
+		"category_data": category_total
 	}
 	db.crawling_log.update_one({"_id": ObjectId(BEFORE_DATA['target'])}, {"$set": log})
 
