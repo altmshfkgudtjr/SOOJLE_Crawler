c5d84954f788d6f132b05fdc0232e5bbb4f20518
NB <altmshfkgudtjr@naver.com>
2020-03-18 13:10:49 +0900

    sj1_main crawler url_hashed duplicate done

--- src/sj_crawling/sj1.py
+++ src/sj_crawling/sj1.py
@@ -68,6 +68,12 @@ def Parsing_post_data(bs, post_url, URL):
 			pass
 		else:
 			img = 1
+			
+	post_url_a = post_url.split("&viewNum=")[0]
+	post_url_b = post_url.split("&viewNum=")[1]
+	while post_url_b[0] != '&':
+		post_url_b = post_url_b[1:]
+	post_url = post_url_a+post_url_b
 
 	#post_data = {'title': ,'author': ,'date': ,'post': ,'tag':[], img:1, 'view':0} 같은 형식
 	post_data['title'] = title.upper()
